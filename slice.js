b630694b88977b73c9683475227e77fd6662a919
95Harpreet <115647828+95Harpreet@users.noreply.github.com>
2022-11-02 19:27:05 +0530

    day 7

---
+++ slice.js
@@ -0,0 +1,4 @@
+var emp=[2,3,45,67,54]
+
+var z=emp.slice(1,4)             //start value,end value
+console.log(z)
---
+++ splice.js
@@ -0,0 +1,4 @@
+var emp=[2,3,45,67,54]
+//emp.splice(3,0,100)    //index,delete values,new value
+emp.splice(3,2,1000)
+console.log(emp)
